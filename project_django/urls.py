aa07f6c38cb7795bd8775fa19f2732193faaa54d
Bonaventura Galang <77723914+bonaventuragal@users.noreply.github.com>
2022-11-08 19:54:13 +0700

    init tailwind

--- project_django/urls.py
+++ project_django/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("__reload__/", include("django_browser_reload.urls")),
     path('', include('example_app.urls')),
 ]
 
